654ffa8d13fc1b8fae61d5966bbb93d9ed7e1f2b
Freddy.Billy <149269152+ImeneInRGB@users.noreply.github.com>
2026-04-20 09:27:37 +0200

    Refactor creation_script.sh for better directory handling
    Update the script to create directories and files in the specified Repo directory. Fix the file creation logic and improve user prompts.

--- creation_script.sh
+++ creation_script.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
+#Complexité: O(N*M) où N est le nombre de fichiers à créer pour chaque préfixe et M est le nombre de préfixes. 
+
+#Cette fonction va créer les fichiers dans un répertoire tout deux spécifiés par l'utilisateur
+mkdir -p Repo
 Creer_repertoire() {
+
+    #je sauvgarde les préfixes dans une liste pour pouvoir les réutiliser plus tard.
     list_prefixes=()
     prefix="defaut"
     i=0
     index=0
+
+    #N et M sont les arguments de la fonction, ils sont passés par l'utilisateur lors de l'appel de la fonction
     N=$1
     M=$2
     echo "Le nom du repertoire que vous voulez creer ? "
     read nom_repertoire
-    mkdir $nom_repertoire
+    mkdir /Repo/$nom_repertoire
+     
+    #j'utilise des boucles pour permettre à l'utilisateur de créer plusieurs fichiers avec différents préfixes
+    #La boucle s'arrêtera lorsque l'utilisateur tapera "stop" pour les préfixes
 
     while [ $prefix != "stop" ];
     do
@@ -20,15 +31,19 @@ Creer_repertoire() {
        ((i++))
 
     done
+    
+    #une fois que l'utilisateur a rentré tout les préfixes, on peut commencer à créer les fichiers
+
     compteur=1
       for ((index=0; index<${#list_prefix[@]}; index++));
       do
         while [ $compteur != $((N+1)) ] && [ ${list_prefix[index]} != "stop" ]
         do
-
+          # Je sauvgarde la date et l'heure de création du fichier pour pouvoir les ajouter au nom du fichier plus tard.
           file_name=$(date +%Y-%T-%3N)
-          touch /${nom_repertoire}/${list_prefix[index]}_${file_name}.txt
-          echo "wow fichier disponible"
+          touch /Repo/${nom_repertoire}/${list_prefix[index]}_${file_name}.txt
+          echo fichier ${compteur} disponible
+
           ((compteur++))
           sleep ${M}s
         done
@@ -41,6 +56,8 @@ Creer_repertoire() {
 
 
 answer="yes"
+
+#le processus se répétera jusqu'à ce que l'utilisateur décide d'arrêter en tapant "non"
 
 echo "Bonjour, ce script va créer des fichers. Introduisez le nombre de fichier N, et l'intervale de création"
 while [ "$answer" != "non" ];
